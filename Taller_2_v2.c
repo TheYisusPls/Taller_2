2b77dff025bae19f205e8faaeeddb527b0e1283a
Jesus Medina <59062804+TheYisusPls@users.noreply.github.com>
2021-04-26 21:31:35 -0400

    Update Taller_2_v2.c

--- Taller_2_v2.c
+++ Taller_2_v2.c
@@ -1,3 +1,4 @@
+//librerias estandar
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -5,25 +6,25 @@
 int opcion;
 
 //opciones menu de frutas
-int opt1_fruta;
-float kilos_fruta;
-float valor_fruta;
+int opt_fruta;
+float kilos_frutas;
+float valor_frutas;
 
 //declaracion de funcion fruta
-float total_por_kilos_frutas(float kilos_fruta, float valor_fruta);
+float total_por_kilos_frutas(float kilos_frutas, float valor_frutas);
 
 
 //opciones menu de verduras
-int opt1_verduras;
-float kilos_verdura;
+int opt_verduras;
+float kilos_verduras;
 float valor_verduras;
 
 //declaracion de funcion verdura
-float total_por_kilos_verduras(float kilos_verdura, float valor_verduras);
+float total_por_kilos_verduras(float kilos_verduras, float valor_verduras);
 
 
-float resultado_verdura;
-float resultado_fruta;
+float resultado_verduras;
+float resultado_frutas;
 float total;
 
 
@@ -33,10 +34,10 @@ int main()
 { 
    
    do{
-    //menu principal    
+    //menu principal   
     printf("\n------------------- \n");
     printf("Bienvenid@ a la Verdureria!\n");
-    printf("¿Que desea llevar? \n");
+    printf("Â¿Que desea llevar? \n");
     printf("------------------- \n");
     printf("(1) Frutas\n");
     printf("(2) Verduras\n");
@@ -48,111 +49,111 @@ int main()
     
     //menu de frutas
     if(opcion == 1){
-    printf("***************\n");
-    printf("Frutas Disponibles: \n");
-    printf("(1): Manzanas\n");
-    printf("(2): Naranjas\n");
-    printf("(3): Platanos\n");
-    printf("(4): Duraznos\n");
-    printf("(5): Uvas\n");
-    printf("***************\n");
-    printf("¿Cual desea llevar?: \n");
-    scanf("%i", &opt1_fruta);
+      printf("***************\n");
+      printf("Frutas Disponibles: \n");
+      printf("(1): Manzanas\n");
+      printf("(2): Naranjas\n");
+      printf("(3): Platanos\n");
+      printf("(4): Duraznos\n");
+      printf("(5): Uvas\n");
+      printf("***************\n");
+      printf("Â¿Cual desea llevar?: \n");
+      scanf("%i", &opt_fruta);
   
 
      
       //opcion de frutas -------->
-      if(opt1_fruta == 1){
-      valor_fruta = 1990;
-      printf("Ha seleccionado Manzanas, el valor del kilogramo de Manzana es $1.990 \n");
-      printf("¿Cuantos kilos desea llevar? \n");
-      scanf("%f",&kilos_fruta);
+      if(opt_fruta == 1){
+        valor_frutas = 1990;
+        printf("Ha seleccionado Manzanas, el valor del kilogramo de Manzana es $1.990 \n");
+        printf("Â¿Cuantos kilos desea llevar? \n");
+        scanf("%f",&kilos_frutas);
       }
       
-      else if(opt1_fruta == 2){
-        valor_fruta = 1590;
+      else if(opt_fruta == 2){
+        valor_frutas = 1590;
         printf("Ha seleccionado Naranjas, el valor del kilogramo de Naranjas es $1.590 \n");
-        printf("¿Cuantos kilos desea llevar? \n");
-        scanf("%f",&kilos_fruta);
+        printf("Â¿Cuantos kilos desea llevar? \n");
+        scanf("%f",&kilos_frutas);
       }
       
-      else if(opt1_fruta == 3){
-        valor_fruta = 1050;
+      else if(opt_fruta == 3){
+        valor_frutas = 1050;
         printf("Ha seleccionado Platano, el valor del kilogramo de Platano es $1.050 \n");
-        printf("¿Cuantos kilos desea llevar? \n");
-        scanf("%f",&kilos_fruta);
+        printf("Â¿Cuantos kilos desea llevar? \n");
+        scanf("%f",&kilos_frutas);
       }
       
-      else if(opt1_fruta == 4){
-        valor_fruta = 2400;
+      else if(opt_fruta == 4){
+        valor_frutas = 2400;
         printf("Ha seleccionado Duraznos, el valor del kilogramo de Duraznos es $2.400 \n");
-        printf("¿Cuantos kilos desea llevar? \n");
-        scanf("%f",&kilos_fruta);
+        printf("Â¿Cuantos kilos desea llevar? \n");
+        scanf("%f",&kilos_frutas);
       }
       
-      else if(opt1_fruta == 5){
-        valor_fruta = 3.580;
+      else if(opt_fruta == 5){
+        valor_frutas = 3580;
         printf("Ha seleccionado Uva, el valor del kilogramo de Uva es $3.580 \n");
-        printf("¿Cuantos kilos desea llevar? \n");
-        scanf("%f",&kilos_fruta);
+        printf("Â¿Cuantos kilos desea llevar? \n");
+        scanf("%f",&kilos_frutas);
       }
 
-    printf("El valor total seria de: $%.1f, ¿Desea llevar algo mas?\n", total_por_kilos_frutas(kilos_fruta,valor_fruta));
+        printf("El valor total seria de: $%.1f, Â¿Desea llevar algo mas?\n", total_por_kilos_frutas(kilos_frutas,valor_frutas));
 
-    resultado_fruta = total_por_kilos_frutas(kilos_fruta, valor_fruta) + resultado_fruta;
+        resultado_frutas = total_por_kilos_frutas(kilos_frutas, valor_frutas) + resultado_frutas;
 }   
 
   //menu de verduras
-  if(opcion == 2){
-  printf("***************\n");
-  printf("Verdudas Disponibles: \n");
-  printf("(1): Lechugas\n");
-  printf("(2): Repollos\n");
-  printf("(3): Zanahorias\n");
-  printf("(4): Cebollas\n");
-  printf("(5): Papas\n");
-  printf("***************\n");
-  printf("¿Cual desea llevar?: \n");
-  scanf("%i", &opt1_verduras);
+    if(opcion == 2){
+      printf("***************\n");
+      printf("Verdudas Disponibles: \n");
+      printf("(1): Lechugas\n");
+      printf("(2): Repollos\n");
+      printf("(3): Zanahorias\n");
+      printf("(4): Cebollas\n");
+      printf("(5): Papas\n");
+      printf("***************\n");
+      printf("Â¿Cual desea llevar?: \n");
+      scanf("%i", &opt_verduras);
 
 	
-    if(opt1_verduras == 1){
-      valor_verduras = 1150;
-      printf("Ha seleccionado Lechugas, el valor de la unidad de Lechugas es $1.150 \n");
-      printf("¿Cuantos kilos desea llevar? \n");
-      scanf("%f",&kilos_verdura);
+      if(opt_verduras == 1){
+        valor_verduras = 1150;
+        printf("Ha seleccionado Lechugas, el valor de la unidad de Lechugas es $1.150 \n");
+        printf("Â¿Cuantos kilos desea llevar? \n");
+        scanf("%f",&kilos_verduras);
     }
     
-    else if(opt1_verduras == 2){
-      valor_verduras = 1190;
-      printf("Ha seleccionado Repollos, el valor de la unidad de Repollos es $1.190 \n");
-      printf("¿Cuantos kilos desea llevar? \n");
-      scanf("%f",&kilos_verdura);
+      else if(opt_verduras == 2){
+        valor_verduras = 1190;
+        printf("Ha seleccionado Repollos, el valor de la unidad de Repollos es $1.190 \n");
+        printf("Â¿Cuantos kilos desea llevar? \n");
+        scanf("%f",&kilos_verduras);
     }
     
-    else if(opt1_verduras == 3){
-      valor_verduras = 890;
-      printf("Ha seleccionado Zanahorias, el valor de un kilogramo de zanahorias es $890 \n");
-      printf("¿Cuantos kilos desea llevar? \n");
-      scanf("%f",&kilos_verdura);
+      else if(opt_verduras == 3){
+        valor_verduras = 890;
+        printf("Ha seleccionado Zanahorias, el valor de un kilogramo de zanahorias es $890 \n");
+        printf("Â¿Cuantos kilos desea llevar? \n");
+        scanf("%f",&kilos_verduras);
     }
     
-    else if(opt1_verduras == 4){
-      valor_verduras = 990;
-      printf("Ha seleccionado Cebollas, el valor del kilogramo de Cebollas es $990 \n");
-      printf("¿Cuantos kilos desea llevar? \n");
-      scanf("%f",&kilos_verdura);
+      else if(opt_verduras == 4){
+        valor_verduras = 990;
+        printf("Ha seleccionado Cebollas, el valor del kilogramo de Cebollas es $990 \n");
+        printf("Â¿Cuantos kilos desea llevar? \n");
+        scanf("%f",&kilos_verduras);
     }
     
-    else if(opt1_verduras == 5){
-      valor_verduras = 1290;
-      printf("Ha seleccionado Papas, el valor del kilogramo de Papas es $1.290\n");
-      printf("¿Cuantos kilos desea llevar? \n");
-      scanf("%f",&kilos_verdura);
+      else if(opt_verduras == 5){
+        valor_verduras = 1290;
+        printf("Ha seleccionado Papas, el valor del kilogramo de Papas es $1.290\n");
+        printf("Â¿Cuantos kilos desea llevar? \n");
+        scanf("%f",&kilos_verduras);
     }
-    printf("El valor total seria de: $%.1f, ¿Desea llevar algo mas?\n",total_por_kilos_verduras(kilos_verdura,valor_verduras));
+        printf("El valor total seria de: $%.1f, Â¿Desea llevar algo mas?\n",total_por_kilos_verduras(kilos_verduras,valor_verduras));
 
-    resultado_verdura = total_por_kilos_verduras(kilos_verdura, valor_verduras) + resultado_verdura;
+        resultado_verduras = total_por_kilos_verduras(kilos_verduras, valor_verduras) + resultado_verduras;
 
 }
 
@@ -160,7 +161,9 @@ int main()
 //fin de programa
 }while(opcion!=9);
   printf("Fin de programa\n");
-  printf("El total es: $%.1f", resultado_fruta+resultado_verdura);
+  printf("El total es: $%.1f", resultado_frutas + resultado_verduras);
+
+  printf("Seleccione la comuna de envio: ");
 
   return 0;
 }
@@ -170,7 +173,7 @@ int main()
 float total_por_kilos_frutas(float kilos_de_fruta, float valor_fruta)
 {
   float x;
-  x = kilos_fruta*valor_fruta;
+  x = kilos_frutas*valor_frutas;
   return x;
 }
 
@@ -179,6 +182,6 @@ float total_por_kilos_frutas(float kilos_de_fruta, float valor_fruta)
 float total_por_kilos_verduras(float kilos_verduras, float valor_verduras)
 {
   float x;
-  x = kilos_verdura*valor_verduras;
+  x = kilos_verduras*valor_verduras;
   return x;
 }
